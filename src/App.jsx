2abe89c95799237145f537df307d4e5f30e33ef3
new1333 <dearest@abouttime.cn>
2024-08-19 00:08:06 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline ">Hello world!</h1>;
+  return (
+    <h1 className="text-blue text-3xl font-bold underline ">Hello world!</h1>
+  );
 }
 
 export default App;
